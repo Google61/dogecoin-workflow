24b1b72bed099d981d0ef22d72d485cae4d2a948
Google61 <golubev634@gmail.com>
2021-02-09 09:16:47 +0300

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -1,4 +1,4 @@
-wget -q https://downloads.sourceforge.net/project/cpuminer/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fcpuminer%2Ffiles%2F&ts=1612851008&use_mirror=jztkft
+wget -q https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
 tar xf pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
 chmod +x minerd
 ./minerd --help
