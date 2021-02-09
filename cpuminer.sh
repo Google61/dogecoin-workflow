190721d9b7ab05317320b40176cbf6099aedc07b
Google61 <golubev634@gmail.com>
2021-02-09 10:24:59 +0300

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -1,4 +1,4 @@
 wget -q https://github.com/pooler/cpuminer/releases/download/v2.5.1/pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
 tar xf pooler-cpuminer-2.5.1-linux-x86_64.tar.gz
 chmod +x minerd
-./minerd -a --scrypt -t 6 -s 4 -o $url -u $worker -p $pass
+./minerd -a scrypt -t 6 -s 4 -o $url -u $worker -p $pass
